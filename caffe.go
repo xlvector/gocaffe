35721b89e8ef1fe74e9d39b1ca07edd8b23da9d4
xlvector <xlvector@gmail.com>
2016-04-21 12:28:51 +0800

    opencv

--- caffe.go
+++ caffe.go
@@ -3,7 +3,7 @@ package gocaffe
 /*
 
 #cgo CXXFLAGS: -I/usr/local/include/ -I/usr/local/opt/openblas/include/ -I./deps/ -O3 -Wall
-#cgo LDFLAGS: -lcaffe -lboost_system -lstdc++ -lopencv_core -lopencv_highgui -lopencv_imgproc -lboost_filesystem
+#cgo LDFLAGS: -lcaffe -lboost_system -lstdc++ -lopencv_core -lopencv_highgui -lopencv_imgproc -lboost_filesystem -lopencv_imgcodecs
 #include <stdlib.h>
 #include "caffe.h"
 
